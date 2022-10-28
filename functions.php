b3320f217c640c6d7219bda778cbf3298e169b2b
Alexander <zhukoff.alexander@gmail.com>
2022-10-28 14:56:37 +0300

    style: Connect scripts, styles, edit paths

---
+++ functions.php
@@ -0,0 +1,13 @@
+<?php
+add_action('wp_enqueue_scripts', 'childhood_styles');
+add_action('wp_enqueue_scripts', 'childhood_scripts');
+
+function childhood_styles() {
+    wp_enqueue_style( 'childhood_style', get_stylesheet_uri() );
+};
+
+function childhood_scripts() {
+    wp_enqueue_script( 'childhood_scripts', get_template_directory_uri() . '/assets/js/main.min.js', 
+    array(), null, true);
+};
+?>
